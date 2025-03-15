359e7a3f12fa86c0273cd3d4c8b152111b6d5871
John Flowers <jeflowers@gmail.com>
2025-03-14 19:11:11 -0500

    Update flask_app.py
    added place holder imports and app route to render template.

--- src/google_interface/flask_app.py
+++ src/google_interface/flask_app.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, jsonify, render_template
+from src.config.config_manager import ConfigManager
+from src.data.document_loader import DocumentLoader
 
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # If you're using a template file
+    # Or if you have the template as a string:
+    # return render_template_string(HTML_TEMPLATE)
+  
